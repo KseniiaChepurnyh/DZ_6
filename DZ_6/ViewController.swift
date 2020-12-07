162434775228d12fd04e8d944d6aa99c7add2b0a
Ксения Чепурных <kseniyache97@gmail.ru>
2020-12-07 14:16:09 +0500

    Initial Commit

---
+++ DZ_6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ_6
+//
+//  Created by Ксения Чепурных on 07.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
